691ee5504f7ad9a7f027ace59652968ce88e0ca3
ankitjena <ankitjena13@gmail.com>
2018-03-11 11:10:08 +0530

    views

---
+++ server/models/users.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+var userSchema = new Schema({
+
+    name : { type : String , required : true},
+    email : { type : String , required : true},
+    contact : { type : String , required : true},
+    designation : { type : String , required : true}
+});
+
+var userModel = mongoose.model('user', userSchema);
+module.exports = userModel;
